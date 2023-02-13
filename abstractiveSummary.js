93c3bc9b3222a4fe95341f0aeadf5550b33086d5
musta55 <bsse1114@iit.du.ac.bd>
2023-02-14 01:39:57 +0600

    one extractive

--- abstractiveSummary.js
+++ abstractiveSummary.js
@@ -1,29 +1,20 @@
-const axios = require('axios');
+let SummarizerManager = require("node-summarizer").SummarizerManager;
+let text_to_summarize = `Image Targets represent images that Vuforia Engine can detect and track. The Engine detects and tracks the image by comparing extracted natural features from the camera image against a known target resource database. Once the Image Target is detected, Vuforia Engine will track the image and augment the content seamlessly using best-in-market image tracking technology.
+Default Observer Event
+Vuforia Engine observes the real world through configured and activated Observers. An Observer creates Observations, that represent information about a target. Observations are collected in the State.
 
-async function generateSummary(text) {
-  const API_KEY = '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';
-  const endpoint = 'https://api.openai.com/v1/engines/davinci/jobs';
+`;
+let number_of_sentences = 3;
+let Summarizer = new SummarizerManager(text_to_summarize,number_of_sentences);
 
-  const response = await axios.post(endpoint, {
-    prompt: `Please summarize the following text: ${text}`,
-    max_tokens: 150, // maximum length of the generated summary
-    n: 1, // number of responses to generate
-    stop: '', // stop sequence for generation
-    temperature: 0.5, // determines the level of randomness in the generated text
-  }, {
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${API_KEY}`,
-    }
-  });
+let summary = Summarizer.getSummaryByFrequency().summary;
 
-  const summary = response.data.choices[0].text;
-  return summary;
-}
 
-// Example usage:
-const text = 'A movement for a more transparent and decentralized Internet is globally attracting more attention. People are becoming more privacy-aware of their online identities and data. The Internet is constantly evolving. Web2 focused on companies that provide services in exchange for personal user data. Web3 commits to user-centricity using decentralization and zero-server architectures. The current digital society demands a global change to empower citizens and take back control. Citizens are locked into big-tech for personal data storage and their for-profit digital identity. Protection of data has proven to be essential, especially due to increased home Internet traffic during the COVID pandemic. Citizens do not possess their own travel documents. The European Commission aims to transition this governmental property towards self-sovereign identity, introducing many new opportunities. Citizens are locked into banks with non-portable IBAN accounts and unsustainable legacy banking infrastructures. Migration to all-digital low-fraud infrastructures and healthier competitive ecosystems is essential. The overall challenge is to return the power to citizens and users again. The transition to a more decentralized Internet is the first crucial step in the realization of user-centricity. This thesis presents the first exploratory study that integrates governmental-issued travel documents into a (decentralized) societal infrastructure. These self-sovereign identities form the authentic base to a private and secure transfer of money and data, and can effectively provide trust in authenticity that is currently missing in online conversations. A fully operational zero-server infrastructure that incorporates all our requirements has been developed for Android using the P2P network overlay IPv8, and a personalized blockchain called TrustChain';
-generateSummary(text).then(summary => {
-  console.log(`Summary: ${summary}`);
-});
+// let summary = Summarizer.getSummaryByRank().then((summary_object)=>{
+//   return summary_object.summary
+// })
 
+// summary.then(values => {
+//   console.log(summary)
+//     });
+  console.log(summary)
